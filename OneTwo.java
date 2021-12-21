46c7d2eeb409dfbecaf70d11efbfb87116038f2b
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-21 09:24:03 +0530

    Add files via upload

---
+++ OneTwo.java
@@ -0,0 +1,12 @@
+public class OneTwo {
+    public static void main(String[] args) {
+        int value = 1;
+        if (value==1){
+            System.out.println("Value is 1");
+        } else if (value ==2){
+            System.out.println("Value is 2");
+        } else {
+            System.out.println("Value neither 1 or 2");
+        }
+    }
+}
